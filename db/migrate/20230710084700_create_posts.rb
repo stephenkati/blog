75af3f0ebbe8b5537cab25e8bf1bfb1767eed883
stephenkati <stephenkatuli88@gmail.com>
2023-07-13 08:06:44 +0300

    update migrations

--- db/migrate/20230710084700_create_posts.rb
+++ db/migrate/20230710084700_create_posts.rb
@@ -3,8 +3,8 @@ class CreatePosts < ActiveRecord::Migration[7.0]
     create_table :posts do |t|
       t.string :title
       t.text :text
-      t.string :comments_counter
-      t.string :likes_counter
+      t.integer :comments_counter, default: 0
+      t.integer :likes_counter, default: 0
       t.references :author, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
