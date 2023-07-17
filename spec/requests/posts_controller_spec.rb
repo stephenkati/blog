708396f7819bef5531569db32fc9b08dcb55aff3
stephenkati <stephenkatuli88@gmail.com>
2023-07-17 16:09:51 +0300

    create requests tests

---
+++ spec/requests/posts_controller_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe PostsController, type: :request do
+  describe 'GET posts#index' do
+    let(:user) { User.create(name: "John", photo: "https://unsplash.com/photo", bio: "bio") }
+    let(:post) { Post.create(title: "Post", text:"My post", author_id: user.id ) }
+
+    it 'returns a success response' do
+      get user_posts_path(user)
+
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders correct template' do
+      get user_posts_path(user)
+
+      expect(response).to render_template(:index)
+    end
+
+    it 'dislays correct template' do
+      get user_posts_path(user)
+
+      expect(response.body).to include("This posts page")
+    end
+  end
+
+  describe 'GET posts#show' do
+    let(:user) { User.create(name: "John", photo: "https://unsplash.com/photo", bio: "bio") }
+    let(:post) { Post.create(title: "Post", text:"My post", author_id: user.id ) }
+
+    it 'returns a success response' do
+      get user_post_path(user, post)
+
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders correct template' do
+      get user_post_path(user, post)
+
+      expect(response).to render_template(:show)
+    end
+
+    it 'dislays correct template' do
+      get user_post_path(user, post)
+
+      expect(response.body).to include("This a post page")
+    end
+  end
+end
---
+++ spec/requests/users_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :request do
+  describe 'Get users#index' do
+    it 'returns a success response' do
+      get users_path
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders correct template' do
+      get users_path
+      expect(response).to render_template(:index)
+    end
+
+    it 'displays correctly' do
+      get users_path
+      expect(response.body).to include("This is users page")
+    end
+  end
+  describe 'Get users#show' do
+    let(:user) { User.create(name: "John", photo: "https://unsplash.com/photo", bio: "bio") }
+
+    it 'returns a success response' do
+      get "/users/#{user.id}"
+
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders correct template' do
+      get "/users/#{user.id}"
+
+      expect(response).to render_template(:show)
+    end
+
+    it 'displays correctly' do
+      get "/users/#{user.id}"
+
+      expect(response.body).to include("This is a user page")
+    end
+  end
+end
