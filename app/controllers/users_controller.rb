80d2ce1edb550e976ee88e148e5b55d9041c3162
stephenkati <stephenkatuli88@gmail.com>
2023-07-18 19:15:07 +0300

    update controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
-  def index; end
-  def show; end
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    @latest_posts = @user.last_three_posts
+  end
 end
