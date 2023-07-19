8547fd5f2774dac0c07d3f6c6ac43f509013ab8a
stephenkati <stephenkatuli88@gmail.com>
2023-07-19 05:47:34 +0300

    Add new methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,27 @@ class PostsController < ApplicationController
     @user = @post.author
     @comments = Post.find(@post.id).comments
   end
+
+  def new
+    @user = current_user
+    @post = Post.new
+  end
+
+  def create
+    @user = current_user
+    @post = Post.new(post_params)
+    @post.author_id = @user.id
+
+    if @post.save
+      redirect_to user_posts_path(@user), notice: 'Post created successfully!'
+    else
+      redirect_to user_posts_path(@user), alert: 'Failed to create the post.'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
