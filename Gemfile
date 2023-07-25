3a143338bd56d5657b37a983090b60e7f8c5cf9c
stephenkati <stephenkatuli88@gmail.com>
2023-07-25 06:40:30 +0300

    Setup devise

--- Gemfile
+++ Gemfile
@@ -74,5 +74,6 @@ end
 gem 'rubocop', '>= 1.0', '< 2.0'
 
 # latest rspec version
+gem 'devise'
 gem 'rails-controller-testing'
 gem 'rspec-rails', '~> 6.0', '>= 6.0.3'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.first
   end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :authenticate_user!
+
+  protected
+
+  def configure_permitted_parameters
+    attributes = [:name]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+  end
 end
