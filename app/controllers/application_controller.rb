2b611b7d92143194403ed96855efcbd01eda5104
Joseck Osugo <josugo26@gmail.com>
2023-07-25 17:37:32 +0300

    Update application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
-  # helper_method :current_user
-  # def current_user
-  #   @current_user ||= User.first
-  # end
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
