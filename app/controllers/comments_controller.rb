29ea3415bdecc1fd10017ee0dc16082c0f6bf154
stephenkati <stephenkatuli88@gmail.com>
2023-07-26 16:03:02 +0300

    Add delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,6 +20,18 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+
+    if @comment.destroy
+      redirect_to user_post_path(@user, @post), notice: 'Comment deleted Successfully'
+    else
+      redirect_to user_post_path(@user, @post), alert: 'Failed to delete comment!'
+    end
+  end
+
   private
 
   def comment_params
