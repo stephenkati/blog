f1dc1fb49f983dcbfccee62ddc272647c339ee24
stephenkati <stephenkatuli88@gmail.com>
2023-07-27 11:17:46 +0300

    Setup api endpoints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!, except: %i[index show]
+  protect_from_forgery with: :exception
+  respond_to :json
 
   protected
 
