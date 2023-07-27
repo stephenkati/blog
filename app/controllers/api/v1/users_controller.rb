4b032b6212788222fb3a3b915aaa5dc336f2c61d
Joseck Osugo <josugo26@gmail.com>
2023-07-27 11:44:36 +0300

    Update users_controller.rb

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::UsersController < ApplicationController
     if user
       render json: { status: 'Success', message: 'User fetched successfully', data: user }, status: :ok
     else
-      render json: user.errors, status: :bad_request
+      render json: { status: 'Error', message: 'Failed to fetch user', errors: user.errors }, status: :bad_request
     end
   end
 end
